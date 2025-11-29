1642417f6d45500aac8d415f190fb794e15b287c
PiQuark6046 <piquark6046@proton.me>
2025-11-29 15:25:38 +0000

    feat: update version property in package.json with the git tag

--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
-import process from 'node:process'
+import * as Process from 'node:process'
+import * as Parsing from '@typescriptprime/parsing'
+import * as Zod from 'zod'
+import * as Semver from 'semver'
+import PackageJson from '@npmcli/package-json'
+import * as Fs from 'node:fs'
 
-const RuntimeArgs = process.argv.slice(2)
+type TParameters = {
+  Tag: string,
+  PackageJsonDirPath: string
+  WorkspacePath: string
+}
 
-console.log(RuntimeArgs)
+let Parameters = (await Parsing.PostProcessing<TParameters>(Parsing.PreProcessing(Process.argv))).Options
+
+await Zod.strictObject({
+  Tag: Zod.stringFormat(Parameters.Tag, Value => Semver.valid(Value) !== null),
+  WorkspacePath: Zod.stringFormat(Parameters.WorkspacePath, Value => Fs.existsSync(Value) && Fs.lstatSync(Value).isDirectory()),
+  PackageJsonDirPath: Zod.stringFormat(Parameters.PackageJsonDirPath,
+    async Value => Fs.existsSync(`${Parameters.WorkspacePath}${Value === '' ? '/package.json' : `/${Value}/package.json`}`)
+    && await PackageJson.load(`${Parameters.WorkspacePath}${Value === '' ? '' : `/${Value}`}`)),
+}).parseAsync(Parameters)
+
+let PackageJsonData = await PackageJson.load(`${Parameters.WorkspacePath}${Parameters.PackageJsonDirPath === '' ? '' : `/${Parameters.PackageJsonDirPath}`}`)
+PackageJsonData.update({
+  version: Semver.clean(Parameters.Tag)
+})
+await PackageJsonData.save()
